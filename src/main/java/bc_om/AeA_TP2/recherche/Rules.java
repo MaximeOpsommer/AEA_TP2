bbc3178741643bc16236921bffeac20d7c9bd48c
Opsommer Maxime <opsommerm@a13p8.fil.univ-lille1.fr>
2017-02-27 17:16:28 +0100

    travail en TP (rien  :D )

---
+++ src/main/java/bc_om/AeA_TP2/recherche/Rules.java
@@ -0,0 +1,12 @@
+package bc_om.AeA_TP2.recherche;
+
+public class Rules {
+
+	public final static int LONGUEUR_MAX = 100;
+	public final static int NOMBRE_APPARIEMENTS_MIN = 24;
+	public final static String[] APPARIEMENTS_AUTORISES = new String[]{"A-U", "C-G", "G-U"};
+	public final static int APPARIEMENTS_SUCCESSIFS_MIN = 3;
+	public final static int LONGUEUR_MAX_BOUCLE_TERMINALE = 8;
+	public final static int LONGUEUR_MAX_BOUCLE_INTERNE = 3;
+	
+}
